1064edfb02a57cba40174c79cf3e8779bbdad58f
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-16 22:59:48 +1100

    Add Display

--- binary_reader/Program.cs
+++ binary_reader/Program.cs
@@ -22,9 +22,26 @@ namespace binary_reader
             using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
             {
                 writer.Write(1.250F);
-                writer.Write(@"C:\Temp);
+                writer.Write(@"C:\Temp");
             }
         }
 
+        public static void Display()
+        {
+            float aspectRatio;
+            string tempDirectory;
+            if (File.Exists(fileName))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                {
+                    aspectRatio = reader.ReadSingle();
+                    tempDirectory = reader.ReadString();
+                }
+
+                Console.WriteLine("Aspect Ratio Set to : " + aspectRatio);
+                Console.WriteLine("Temp Directory is : " + tempDirectory);
+                Console.Read();
+            }
+        }
     }
 }
