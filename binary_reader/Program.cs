6080aff172296ef7dbed0767f2a702cb8b7d67bc
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-16 22:50:07 +1100

    Initial commit

---
+++ binary_reader/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * C# Program to Implement BinaryReader 
+ */
+using System;
+using System.IO;
+
+namespace binary_reader
+{
+    class ConsoleApplication
+    {
+        const string fileName = "program.dat";
+
+        static void Main()
+        {
+            Write();
+            Console.WriteLine("Using Binary Writer Class the Contents are Written");
+            Display();
+        }
+
+    }
+}
